f18902414ed976f980d78f1f743ba2eb456a7e37
MikkelBeier <MikkelBeier@hotmail.com>
2020-01-05 12:44:17 +0100

    asd

--- src/OOP/Polymorphism/Main.java
+++ src/OOP/Polymorphism/Main.java
@@ -9,5 +9,13 @@ class Main {
         System.out.println(Math.Multiply(2, 4));
         System.out.println(Math.Multiply(5.5, 6.3));
 
+        Salary s = new Salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+        Employee e = new Salary("John Adams", "Boston, MA", 2, 2400.00);
+        System.out.println("Call mailCheck using Salary reference --");
+        s.mailCheck();
+        System.out.println("\n Call mailCheck using Employee reference--");
+        e.mailCheck();
+
+
     }
 }
